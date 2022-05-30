6384c404ede9f22da7c4e6c8e353681dc1463936
Mazaherian <ftmhmzhrn@gmail.com>
2022-05-30 15:03:38 +0430

    contactFiedChanged

--- MyPortfolio/Models/Contact.cs
+++ MyPortfolio/Models/Contact.cs
@@ -5,7 +5,7 @@
         //public string Key { get; set; }
         //public string Value { get; set; }
         public string Email { get; set; }
-        public string Mobile { get; set; }
+        public string MobileNumber { get; set; }
         public string Github { get; set; }
     }
 }
